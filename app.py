45809adaa7d77b2db6c4d75304c480d55d6a6db6
martin <riembault.martin@gmail.com>
2025-03-16 02:43:33 +0100

    requirements

--- app.py
+++ app.py
@@ -177,4 +177,4 @@ def user_moods(username):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
